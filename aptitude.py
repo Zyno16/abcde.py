e73ff37482a818da25b202debfd6a2e7797b40c7
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2024-03-10 09:14:49 +0000

    Add files via upload

---
+++ aptitude.py
@@ -0,0 +1,26 @@
+
+n =int(input("enter the word: "))
+c =65
+for i in range(1,n+1):
+    #c =65
+    for j in range(i):
+        
+        print(chr(c),end="")
+        c +=1
+    print()
+
+    
+    
+
+    
+
+       
+
+
+        
+    
+
+
+
+ 
+
